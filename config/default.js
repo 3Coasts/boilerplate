a1cc8b954f3324b02fa8b3aa5f58fbdb30a8386e
Brian Berlin <bri@nberl.in>
2015-05-22 15:31:51 -0500

    example config files

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  version: '1', // used for api version in uri
+  environment: 'development', // or 'production;
+  authTokenTimeout: 60000, // Time before authTokens are deleted
+  debug: true, // Used to print messages to console
+  port: 3000
+};
---
+++ config/private.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mongo: {
+    host: '',
+    database: '',
+    user: '',
+    pass: '',
+  },
+  twilio: {
+    number: '',
+    sid: '',
+    token: ''
+  }
+};
