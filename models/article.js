6eaa94c4c0dae47ef796f406f96c680a6eb0a4f5
Brian Berlin <bri@nberl.in>
2015-06-03 00:30:49 -0500

    refactor cleanup

--- models/article.js
+++ models/article.js
@@ -1,1 +1,22 @@
-//model stub
+/*
+module.exports = {
+  hooks: {
+    beforeValidate: function(values, cb) {},
+    afterValidate: function(values, cb) {},
+    beforeCreate: function(values, cb) {},
+    afterCreate: function(newlyInsertedRecord, cb) {},
+    beforeValidate: function(valuesToUpdate, cb){},
+    afterValidate: function(valuesToUpdate, cb){},
+    beforeUpdate: function(valuesToUpdate, cb){},
+    afterUpdate: function(updatedRecord, cb){},
+    beforeDestroy: function(criteria, cb){},
+    afterDestroy: function(deletedRecord, cb){}
+  },
+  classMethods: {
+    methodName: function(data, cb) {}
+  },
+  instanceMethods: {
+    methodName: function(cb) {}
+  }
+};
+*/
