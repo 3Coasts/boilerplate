1a5e056c519018c8cca67a08d409bc6a17887c18
Brian Berlin <bri@nberl.in>
2015-06-07 20:33:52 -0500

    added search to plugin views

--- routes/article.js
+++
@@ -1,9 +0,0 @@
-var models = require('../lib/app').models;
-
-exports.postArticle = function postArticle(req, res, next) {
-  req.body.author = req.user;
-  models.article.create(req.body, function(err, model) {
-    if (err) return next(err);
-    return res.json(model);
-  })
-};
